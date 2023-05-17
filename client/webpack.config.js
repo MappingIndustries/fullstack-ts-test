ee99abfe97be4d617e4039fbfc9e50e15719a8cd
AlexisFlach <Alexis.Flach87@gmail.com>
2023-05-17 07:28:24 +0200

    Setup client environment with React, TypeScript, Webpack, Prettier,  and Docker

---
+++ client/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+  },
+}
