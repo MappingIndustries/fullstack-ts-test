1d64288b4c9011ad0481e3cd4a87aeaf9c30ec2c
AlexisFlach <Alexis.Flach87@gmail.com>
2023-05-25 12:00:27 +0200

    Clean up client

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Register } from '../components/Auth/Register';
 
 const Home: React.FC = () => {
-    const [showLogin, setShowLogin] = useState(true);
-
     return (
         <div className='row mt-5'>
             <div className='col-md-6 offset-md-3'>
                 <h1 className='text-center'>Welcome to The Quotes</h1>
-                {showLogin && <Register />}
+                <Register />
             </div>
         </div>
     );
