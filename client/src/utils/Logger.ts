6f75556bd7e0abe93933ed3eea704eee186d9a7b
AlexisFlach <Alexis.Flach87@gmail.com>
2023-05-25 10:46:56 +0200

    Refactor Logger to console log messages

--- client/src/utils/Logger.ts
+++ client/src/utils/Logger.ts
@@ -1,15 +1,7 @@
-import fs from 'fs'
-
 export class Logger {
-  static logFilePath = './logs.txt'
-
   static error(message: any, error: any) {
     const timestamp = new Date().toISOString()
     const logMessage = `[${timestamp}] ERROR: ${message}, ${error.message}\n`
-    fs.appendFile(this.logFilePath, logMessage, (err: any) => {
-      if (err) {
-        console.error('Failed to write to log file:', err)
-      }
-    })
+    console.error(logMessage)
   }
 }
